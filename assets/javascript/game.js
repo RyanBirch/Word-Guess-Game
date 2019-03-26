98c4b91e45839b1ef74672c41c44fdc97e9c578a
Ryan Birch <ryan.birch@gmail.com>
2019-03-26 01:20:24 -0400

    fixed bugs in game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,18 @@ let gameStatus = document.getElementById("game-status")
 // initial game state
 let wins = 0
 winsText.textContent += wins
+let guessesRemaining = 10
+guessesRemainingText.textContent = guessesRemaining
+let currentWord = wordArray[Math.floor(Math.random() * wordArray.length)]
+let currentPlace = 0
+currentWordText.textContent = ""
+for (let i = 0; i < currentWord.length; i++) {
+  currentWordText.textContent += "_ "
+}
+lettersGuessedText.textContent = ""
+gameStatus.textContent = ""
+let count = 0
+let usedLetters = []
 
 
 // resets the game after a win or loss
@@ -23,12 +35,64 @@ function reset() {
   currentWord = wordArray[Math.floor(Math.random() * wordArray.length)]
   currentPlace = 0
   currentWordText.textContent = ""
+  for (let i = 0; i < currentWord.length; i++) {
+    currentWordText.textContent += "_ "
+  }
   lettersGuessedText.textContent = ""
   gameStatus.textContent = ""
+  count = 0
+  usedLetters = []
+}
+
+// checks to see if chosen letter is in the word
+function isCorrect(action, currentWord) {
+  for (let i = 0; i < currentWord.length; i++) {
+    if (action === currentWord[i])
+      return true
+  }
+
+  return false
+}
+
+// increments letter count to determine if all letters have been guessed
+function letterCount(action, currentWord) {
+  let count = 0
+
+  for (let i = 0; i < currentWord.length; i++) {
+    if (action === currentWord[i])
+      count++
+  }
+
+  return count
+}
+
+// checks to see if chosen letter has already been chosen
+function unused(action, usedLetters) {
+  for (let i = 0; i < usedLetters.length; i++) {
+    if (action === usedLetters[i])
+      return false
+  }
+
+  return true
+}
+
+// reveals letters that were guessed correctly
+function reveal(action, currentWord, currentWordText, usedLetters) {
+  currentWordText.textContent = ""
+
+  for (let i = 0; i < currentWord.length; i++) {
+
+    if (usedLetters.includes(currentWord[i]))
+      currentWordText.textContent += currentWord[i]
+    else
+      currentWordText.textContent += " _"
+  }
+
 }
 
 // start a fresh game
 reset()
+
 
 document.onkeyup = function(event) {
 
@@ -36,12 +100,14 @@ document.onkeyup = function(event) {
   if (guessesRemaining > 0 && currentPlace !== currentWord.length) {
     let action = event.key
 
-    // player guesses right
-    if (action === currentWord[currentPlace]) {
-      currentWordText.textContent += currentWord[currentPlace]
-      currentPlace++
+    //player guesses right
+    if (isCorrect(action, currentWord) && unused(action, usedLetters)) {
+      usedLetters.push(action)
+      count = letterCount(action, currentWord)
+      currentPlace += count
+      reveal(action, currentWord, currentWordText, usedLetters)
 
-      // player wins
+      //player wins
       if (currentPlace === currentWord.length) {
         gameStatus.textContent = "You win! Press any key to play again"
         wins++
@@ -60,13 +126,6 @@ document.onkeyup = function(event) {
     lettersGuessedText.textContent += " " + action
   }
 
-  // player wins and has option to reset
-  else if (currentPlace === currentWord.length) {
-    reset()
-  }
-
-  // player has no guesses remaining and game ends
-  else {
-    reset()
-  }
+  // player has option to reset when game ends
+  else reset()
 }
